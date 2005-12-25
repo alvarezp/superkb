6f044a8c1af582ba3017db862369ed523af2daf8
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2005-12-25 03:20:22 +0000

    Updating some files. Added main.c and moved some declarations out of superkb.c

--- src/superkb.c
+++ src/superkb.c
@@ -9,6 +9,8 @@
 #include <sys/time.h>
 #include <errno.h>
 
+#include "superkb.h"
+
 #define       timerisset(tvp)\
               ((tvp)->tv_sec || (tvp)->tv_usec)
 #define       timercmp(tvp, uvp, cmp)\
@@ -18,79 +20,64 @@
 #define       timerclear(tvp)\
               ((tvp)->tv_sec = (tvp)->tv_usec = 0)
 
-struct config {
-  void (*_DrawKeyboard)(Display *, Window, GC, Pixmap, int, int, int);
+/* Wrappers for easy dynamic array element adding and removing. */
+#define list_add_element(x, xn, y) {x = (y *)realloc(x, (++(xn))*sizeof(y));}
+#define list_rmv_element(x, xn, y) {x = (y *)realloc(x, (--(xn))*sizeof(y));}
+
+struct _kbwin {
+    void (*init) (Display *);
+    void (*map) (Display *);
+    void (*unmap) (Display *);
+    void (*draw) (Display *);
+    void (*event) (Display *, XEvent ev);
 };
 
-struct config c;
+struct config {
+    char kblayout[3];
+    KeySym key1;
+    KeySym key2;
+};
 
-void DrawKeyboard(Display * dpy, Window w, GC gc, Pixmap kb_pix, int pmw,
-		  int pmh, int ix1)
-{
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, 0, 0);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, pmw * 1.8, 0);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, pmw * 1.8 + pmw, 0);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, pmw * 1.8 + 2 * pmw, 0);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, pmw * 1.8 + 3 * pmw, 0);
+struct instance {
+    KeyCode key1;
+    KeyCode key2;
+    Display *dpy;
+    Window rootwin;
+};
 
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, 0, pmh * 1.6);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, pmw, pmh * 1.6);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, pmw * 2, pmh * 1.6);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, pmw * 3, pmh * 1.6);
+struct kb *kb = NULL;
+unsigned int kb_n = 0;
 
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, 0, pmh * 1.6 + pmh);
-    XCopyArea(dpy, kb_pix, w, gc, ix1 + 1, 0, pmw, pmh, ix1 * 4,
-	      pmh * 1.6 + pmh);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh,
-	      ix1 * 3 + pmw - 1, pmh * 1.6 + pmh);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh,
-	      ix1 * 3 + pmw * 2 - 1, pmh * 1.6 + pmh);
+struct _kbwin kbwin = { NULL, NULL, NULL, NULL, NULL };
+struct config conf = { "", 0, 0};
+struct instance inst = { 0, 0, NULL, 0 };
 
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, 0, pmh * 1.6 + pmh * 2);
-    XCopyArea(dpy, kb_pix, w, gc, ix1 + 1, 0, pmw, pmh,
-	      ix1 * 8, pmh * 1.6 + pmh * 2);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh,
-	      ix1 * 7 + pmw - 1, pmh * 1.6 + pmh * 2);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh,
-	      ix1 * 7 + pmw * 2 - 1, pmh * 1.6 + pmh * 2);
-
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh, 0, pmh * 1.6 + pmh * 3);
-    XCopyArea(dpy, kb_pix, w, gc, ix1 + 1, 0, pmw, pmh,
-	      ix1 * 8, pmh * 1.6 + pmh * 3);
-    XCopyArea(dpy, kb_pix, w, gc, ix1 + 1, 0, pmw, pmh,
-	      ix1 * 12, pmh * 1.6 + pmh * 3);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh,
-	      ix1 * 11 + pmw - 1, pmh * 1.6 + pmh * 3);
-    XCopyArea(dpy, kb_pix, w, gc, 0, 0, pmw, pmh,
-	      ix1 * 11 + pmw * 2 - 1, pmh * 1.6 + pmh * 3);
-
-    XDrawString(dpy, w, gc, ix1 * 7 + pmw + pmw / 2,
-		pmh * 1.6 + pmh * 2 + pmh / 2, "A", 1);
-    XDrawString(dpy, w, gc, 5, 295,
-		"Hold down combination to reconfigure.",
-		strlen("Hold down combination to reconfigure."));
-    XFlush(dpy);
-}
-
-void superkb_init()
-{
-
-}
-
-static void timerdiff(struct timeval *dst, struct timeval *tv0,
-		      struct timeval *tv1)
+static void
+timerdiff(struct timeval *dst, struct timeval *tv0, struct timeval *tv1)
 {
     if (tv1->tv_usec >= tv0->tv_usec) {
-	dst->tv_usec = tv1->tv_usec - tv0->tv_usec;
-	dst->tv_sec = tv1->tv_sec - tv0->tv_sec;
+        dst->tv_usec = tv1->tv_usec - tv0->tv_usec;
+        dst->tv_sec = tv1->tv_sec - tv0->tv_sec;
     } else {
-	dst->tv_usec = tv1->tv_usec - tv0->tv_usec + 1000000;
-	dst->tv_sec = tv1->tv_sec - tv0->tv_usec - 1;
+        dst->tv_usec = tv1->tv_usec - tv0->tv_usec + 1000000;
+        dst->tv_sec = tv1->tv_sec - tv0->tv_usec - 1;
     }
 }
 
-int XNextEventWithTimeout(Display * display, XEvent * event_return,
-			  struct timeval *tv)
+/* This function is the same as XNextEvent but a timeout can be
+ * specified in "to". It's supposed to accept NULL for no timeout
+ * as the function works like a wrapper for select().
+ *
+ * It works by select()ing on XConnectionNumber(display) to wait either
+ * for incoming traffic or timeout. If the former is found, return the
+ * next event.
+ *
+ * RETURNS <= 0 ? Timed out before getting an event.
+ * RETURNS > 0 ? Event catched.
+ */
+int
+XNextEventWithTimeout(Display * display, XEvent * event_return,
+                      struct timeval *to)
 {
     fd_set fd;
     int r;
@@ -98,158 +85,182 @@ int XNextEventWithTimeout(Display * display, XEvent * event_return,
     FD_ZERO(&fd);
     FD_SET(XConnectionNumber(display), &fd);
 
-    r = select(FD_SETSIZE, &fd, NULL, NULL, tv);
+    r = select(FD_SETSIZE, &fd, NULL, NULL, to);
 
     if (r <= 0) {
-	/* Timeout */
-	return r;
+        /* Timeout */
+        return r;
     }
 
     XNextEvent(display, event_return);
     return r;
 }
 
-int main()
+void
+superkb_addkb(KeySym keysym, unsigned int state,
+               enum action_type action_type, const char *command)
 {
+    list_add_element(kb, kb_n, struct kb);
+    kb[kb_n - 1].keycode = XKeysymToKeycode(inst.dpy, keysym);
+    kb[kb_n - 1].state = state;
+    kb[kb_n - 1].action_type = action_type;
+    strcpy(kb[kb_n - 1].command, command);
+}
 
-    /* Conectar e inicializar */
-    Display *dpy;
-    Window root;
-    if (!(dpy = XOpenDisplay(NULL)))
-	return 1;
-    Font f = XLoadFont(dpy, "*-bitstream vera sans-bold-r-*");
-    Colormap cm = DefaultColormap(dpy, 0);
-    root = DefaultRootWindow(dpy);
-
-    KeyCode super_l = XKeysymToKeycode(dpy, XStringToKeysym("Super_L"));
-    KeyCode super_r = XKeysymToKeycode(dpy, XStringToKeysym("Super_R"));
-    KeyCode KB_A = XKeysymToKeycode(dpy, XStringToKeysym("A"));
-
-    unsigned long black = BlackPixel(dpy, DefaultScreen(dpy));
-    unsigned long white = WhitePixel(dpy, DefaultScreen(dpy));
-    unsigned long bgcolor = (0 << 16) + (20 << 8) + 220;
-
-    /* Establecer la función de pintado del teclado */
-    _DrawKeyboard=DrawKeyboard;
-
-    /* Crear la ventana con el tecladito: w */
-    Window w;
-    XSetWindowAttributes attr;
-
-    w = XCreateSimpleWindow(dpy, root, 400, 400, 400, 300, 0, white,
-			    bgcolor);
-    assert(w);
-    attr.override_redirect = True;	/* Adios WM */
-    XChangeWindowAttributes(dpy, w, CWOverrideRedirect, &attr);
-
-    /* Crearle un contexto grafico a w. */
-    GC gc = XCreateGC(dpy, w, 0, NULL);
-    XSetFont(dpy, gc, f);
-
+void superkb_start()
+{
     /* Solicitar los eventos */
-    XGrabKey(dpy, super_l, AnyModifier, root, True, GrabModeAsync,
-	     GrabModeAsync);
-    XGrabKey(dpy, super_r, AnyModifier, root, True, GrabModeAsync,
-	     GrabModeAsync);
-    XSelectInput(dpy, w, ExposureMask);
-
-    /* Crear el pixmap de la teclita */
-    int pmw = 45, pmh = 45;
-    int ox1 = 0, oy1 = 0, ox2 = 44, oy2 = 44;
-    int ix1 = 4, iy1 = 2, ix2 = 40, iy2 = 38;
-    Screen *s = XDefaultScreenOfDisplay(dpy);
-    Pixmap kb_pix =
-	XCreatePixmap(dpy, w, pmw, pmh, XDefaultDepthOfScreen(s));
-    assert(kb_pix);
-
-    XSetForeground(dpy, gc, black);
-    XFillRectangle(dpy, kb_pix, gc, 0, 0, pmw, pmh);
-    XSetForeground(dpy, gc, white);
-    XDrawRectangle(dpy, kb_pix, gc, ox1, oy1, ox2 - ox1, oy2 - oy1);	/* outer box */
-    XDrawRectangle(dpy, kb_pix, gc, ix1, iy1, ix2 - ix1, iy2 - iy1);	/* inner box */
-    XDrawLine(dpy, kb_pix, gc, ox1, oy1, ix1, iy1);	/* rest of lines */
-    XDrawLine(dpy, kb_pix, gc, ox1, oy2, ix1, iy2);
-    XDrawLine(dpy, kb_pix, gc, ox2, oy1, ix2, iy1);
-    XDrawLine(dpy, kb_pix, gc, ox2, oy2, ix2, iy2);
-
-    XFlush(dpy);
+    if (inst.key1)
+        XGrabKey(inst.dpy, inst.key1, AnyModifier, inst.rootwin, True,
+                 GrabModeAsync, GrabModeAsync);
+    if (inst.key2)
+        XGrabKey(inst.dpy, inst.key2, AnyModifier, inst.rootwin, True,
+                 GrabModeAsync, GrabModeAsync);
 
     struct timeval to = { 0, 0 };
     int ignore_release = 0;
+    int saved_autorepeat_mode = 0;
+
+    /* Counter, in case user presses both Supers and releases only one. */
+    int super_was_active = 0;
+
     for (;;) {
 
-	int still_pressed = 0;
-	int saved_autorepeat_mode;
-	struct timeval hold_start;
-	struct timeval hold_end;
-	XEvent ev;
-	int timed_out;
+        struct timeval hold_start;
+        struct timeval hold_end;
+        XEvent ev;
+        int timed_out;
 
-	timed_out = 0;
+        timed_out = 0;
 
-	if (!timerisset(&to))
-	    XNextEvent(dpy, &ev);
+        /* Decide wether to use XNextEvent or my own XNextEventWithTimeout
+         * and do accordingly. If WithTimeout was used, substract the
+         * time actually elapsed to the set timeout. */
+        if (!timerisset(&to))
+        {
+            printf("XNE: %d %d\n", to.tv_sec, to.tv_usec);
+            XNextEvent(inst.dpy, &ev);
+        }
+        else {
+            printf("XNEWT: %d %d\n", to.tv_sec, to.tv_usec);
+            gettimeofday(&hold_start, NULL);
 
-	else {
-	    gettimeofday(&hold_start, NULL);
+            if (XNextEventWithTimeout(inst.dpy, &ev, &to) <= 0)
+                /* Timed out */
+                timed_out = 1;
+            else
+                timed_out = 0;
 
-	    if (!XNextEventWithTimeout(dpy, &ev, &to))
-		/* Timed out */
-		timed_out = 1;
-	    else
-		timed_out = 0;
+            gettimeofday(&hold_end, NULL);
+            timerdiff(&to, &hold_start, &hold_end);
+        }
 
-	    gettimeofday(&hold_end, NULL);
-	    timerdiff(&to, &hold_start, &hold_end);
-	}
+        if (timed_out) {
+            printf("CONFIGURE BINDING!\n");
+            timerclear(&to);
+            ignore_release = 1;
+        } else if (ev.xany.window != inst.rootwin) {
+            kbwin.event(inst.dpy, ev);
+        } else if (ev.xkey.keycode == inst.key1
+                   || ev.xkey.keycode == inst.key2) {
+            if (ev.type == KeyPress) {
+                ignore_release = 0;
 
-	if (timed_out) {
-	    XDrawString(dpy, w, gc, 5, 275,
-			"User asked for MENU+A configuration.",
-			strlen("User asked for MENU+A configuration."));
-	    printf("CONFIGURE BINDING!\n");
-	    timerclear(&to);
-	    ignore_release = 1;
-	} else if (ev.type == KeyPress && ev.xkey.subwindow != None) {
-	    if (ev.xkey.keycode == super_l || ev.xkey.keycode == super_r) {
-		ignore_release = 0;
+                if (super_was_active++)
+                    continue;
 
-		XMapWindow(dpy, w);
-		XKeyboardState xkbs;
-		XGetKeyboardControl(dpy, &xkbs);
-		saved_autorepeat_mode = xkbs.global_auto_repeat;
-		XKeyboardControl xkbc;
-		xkbc.auto_repeat_mode = AutoRepeatModeOff;
-		XChangeKeyboardControl(dpy, KBAutoRepeatMode, &xkbc);
+                XKeyboardState xkbs;
 
-		XGrabKey(dpy, KB_A, Mod4Mask, root, True,
-			 GrabModeAsync, GrabModeAsync);
-	    } else {
-		to.tv_sec = 3;
-		to.tv_usec = 0;
-	    }
-	} else if (ev.type == KeyRelease && ev.xkey.subwindow != None) {
-	    XKeyboardControl xkbc;
-	    xkbc.auto_repeat_mode = AutoRepeatModeOn;
-	    XChangeKeyboardControl(dpy, KBAutoRepeatMode, &xkbc);
+                /* Save autorepeat previous state. Then turn off. */
+                XGetKeyboardControl(inst.dpy, &xkbs);
+                saved_autorepeat_mode = xkbs.global_auto_repeat;
 
-	    if (!ignore_release) {
-		ignore_release = 1;
+                XKeyboardControl xkbc;
+                xkbc.auto_repeat_mode = AutoRepeatModeOff;
+                XChangeKeyboardControl(inst.dpy, KBAutoRepeatMode, &xkbc);
 
-                /* pseudo-code: if (action_key == KB_A) { */
-		XDrawString(dpy, w, gc, 5, 275,
-			    "User asked to run gedit.",
-			    strlen("User asked to run gedit."));
-		printf("RUN APP!\n");
-		/*system("/usr/bin/gedit");*/
-		timerclear(&to);
-		/* } */
-	    }
-	    XUngrabKey(dpy, KB_A, Mod4Mask, root);
-	    XUnmapWindow(dpy, w);
-	} else if (ev.type == Expose) {
-	    _DrawKeyboard(dpyg, w, gc, kb_pix, pmw, pmh, ix1);
-	} else
-	    XAllowEvents(dpy, ReplayKeyboard, CurrentTime);
+                /* Grab the keyboard. */
+                XGrabKeyboard(inst.dpy, inst.rootwin, False, GrabModeAsync,
+                              GrabModeAsync, CurrentTime);
+
+                /* Map Window. */
+                kbwin.map(inst.dpy);
+
+            } else if (ev.type == KeyRelease) {
+                if (--super_was_active)
+                    continue;
+
+                /* Restore saved_autorepeat_mode. */
+                XKeyboardControl xkbc;
+                /*xkbc.auto_repeat_mode = saved_autorepeat_mode;*/
+                xkbc.auto_repeat_mode = AutoRepeatModeOn;
+                XChangeKeyboardControl(inst.dpy, KBAutoRepeatMode, &xkbc);
+
+                XUngrabKeyboard(inst.dpy, CurrentTime);
+                kbwin.unmap(inst.dpy);
+            }
+        } else if (ev.type == KeyPress) {
+            to.tv_sec = 3;
+            to.tv_usec = 0;
+        } else if (ev.type == KeyRelease && !ignore_release) {
+            /* User might have asked for binding configuration, so ignore key
+             * release.
+             */
+            int i;
+            timerclear(&to);
+            printf("KeyRelease: %d %d\n", kb[0].keycode, kb[0].state);
+            printf("KeyRelease: %d %d\n", ev.xkey.keycode, ev.xkey.state);
+            for (i = 0; i < kb_n; i++) {
+                if (kb[i].keycode == ev.xkey.keycode &&
+                    kb[i].state == (ev.xkey.state & kb[i].state)) {
+                    switch (kb[i].action_type) {
+                    case AT_COMMAND:
+                        system(kb[kb_n - 1].command);
+                        break;
+                    }
+                }
+            }
+
+        } else {
+            /* According to manual, this should not be necessary. */
+            /* XAllowEvents(inst.dpy, ReplayKeyboard, CurrentTime); */
+        }
     }
+}
+
+int superkb_load(char *display,
+                 void (*kbwin_init) (Display *),
+                 void (*kbwin_map) (Display *),
+                 void (*kbwin_unmap) (Display *),
+                 void (*kbwin_draw) (Display *),
+                 void (*kbwin_event) (Display *, XEvent ev),
+                 const char *kblayout, KeySym key1, KeySym key2)
+{
+    /* FIXME: Validate parameters. */
+
+    /* Set configuration values. Parameters should be already validated. */
+    kbwin.init = kbwin_init;
+    kbwin.map = kbwin_map;
+    kbwin.unmap = kbwin_unmap;
+    kbwin.draw = kbwin_draw;
+    kbwin.event = kbwin_event;
+    strcpy(conf.kblayout, kblayout);
+    conf.key1 = key1;
+    conf.key2 = key2;
+
+
+    /* 2. Connect to display. */
+    if (!(inst.dpy = XOpenDisplay(display)))
+        return 1;
+
+    inst.key1 = XKeysymToKeycode(inst.dpy, key1);
+    inst.key2 = XKeysymToKeycode(inst.dpy, key2);
+
+    inst.rootwin = DefaultRootWindow(inst.dpy);
+
+    /* Create the keyboard window. */
+    kbwin.init(inst.dpy);
+
+    XFlush(inst.dpy);
+
 }
