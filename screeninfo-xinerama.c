730d30f99fa9a1e39604fd9fd7435f7dc24be397
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2010-01-07 15:02:08 -0800

    Fix Xinerama detection at runtime.
    The problem was in the logic, namely the comparison
    which got carried from the module reorganization.
    
    The change is done in such a way that instead of
    
    if (Xinerama_works) then { do_it(); }
    
    now it is
    
    if (!Xinerama_works) then { fallback; return; }
    if (!Xinerama_query) then { fallback; return; }
    /* We know Xinerama worked. */
    
    and so, it is primarily just movement of code (implies
    indentation) and fixing of conditionals.

--- screeninfo-xinerama.c
+++ screeninfo-xinerama.c
@@ -31,34 +31,10 @@ void screeninfo_get_screens(Display *dpy, screeninfo_t **screens, int *screens_n
 	xinerama_queryversion_result = XineramaQueryVersion (dpy, &dummyint, &dummyint);
 	xinerama_is_active = XineramaIsActive(dpy);
 
-	if (xinerama_queryextension_result == True
-		&& xinerama_queryversion_result != 0
-		&& xinerama_is_active == True
-		&& xinerama_screens_n) {
-
-		xinerama_screens = XineramaQueryScreens (dpy, &xinerama_screens_n);
-
-		if (xinerama_screens == NULL)
-
-		/* Everything went well */
-
-		debug(3, "[xs] Xinerama detected %d screens.\n", xinerama_screens_n);
-		int i;
-		for (i=0; i < xinerama_screens_n; i++) {
-#define xsi ((xinerama_screens)[i])
-			debug (4, "[xs] Screen #%d: number=%d, x=%d, y=%d, w=%d, h=%d\n",
-				i, xsi.screen_number, xsi.x_org, xsi.y_org, xsi.width, xsi.height);
-#undef xsi
-		}
-
-		/* We should dump all information from Xinerama to screeninfo. However,
-		 * given the struct is the same (intentionally) we are going to simply
-		 * memcpy() it. If the structure ever changes, this code should be
-		 * definitely updated.
-		 */
-		memcpy(screens, xinerama_screens, sizeof(screeninfo_t)*xinerama_screens_n);
-
-	} else {
+	if (xinerama_queryextension_result != True
+		|| xinerama_queryversion_result == 0
+		|| xinerama_is_active != True)
+	{
 		fprintf(stderr, "superkb: Xinerama extension not available! Falling back.\n");
 		*screens = (screeninfo_t *) malloc(sizeof(screeninfo_t));
 		*screens_n = 1;
@@ -68,7 +44,43 @@ void screeninfo_get_screens(Display *dpy, screeninfo_t **screens, int *screens_n
 		screens[0]->y_org = 0;
 		screens[0]->width = DisplayWidth(dpy, DefaultScreen(dpy));
 		screens[0]->height = DisplayHeight(dpy, DefaultScreen(dpy));
+
+		return;
 	}
+
+	xinerama_screens = XineramaQueryScreens (dpy, &xinerama_screens_n);
+
+	if (xinerama_screens_n == 0) {
+		fprintf(stderr, "superkb: Xinerama extension available, but query failed! Falling back.\n");
+		*screens = (screeninfo_t *) malloc(sizeof(screeninfo_t));
+		*screens_n = 1;
+
+		screens[0]->screen_number = 0;
+		screens[0]->x_org = 0;
+		screens[0]->y_org = 0;
+		screens[0]->width = DisplayWidth(dpy, DefaultScreen(dpy));
+		screens[0]->height = DisplayHeight(dpy, DefaultScreen(dpy));
+
+		return;
+	}
+
+	/* Everything went well */
+
+	debug(3, "[xs] Xinerama detected %d screens.\n", xinerama_screens_n);
+	int i;
+	for (i=0; i < xinerama_screens_n; i++) {
+#define xsi ((xinerama_screens)[i])
+		debug (4, "[xs] Screen #%d: number=%d, x=%d, y=%d, w=%d, h=%d\n",
+			i, xsi.screen_number, xsi.x_org, xsi.y_org, xsi.width, xsi.height);
+#undef xsi
+	}
+
+	/* We should dump all information from Xinerama to screeninfo. However,
+	 * given the struct is the same (intentionally) we are going to simply
+	 * memcpy() it. If the structure ever changes, this code should be
+	 * definitely updated.
+	 */
+	memcpy(screens, xinerama_screens, sizeof(screeninfo_t)*xinerama_screens_n);
 
 }
 
