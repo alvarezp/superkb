cf9a9bb303036085180181808078abe6ac652aff
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2019-05-22 21:40:44 -0500

    fix: added forgotten main-help-message.h
    This means we broke compilation since commit b364c898: "Moved
    the help text outside of main.c". In that commit we moved the
    help message from main.c into main-help-message.h but forgot
    the file.
    
    This commit adds it as it should be.

---
+++ main-help-message.h
@@ -0,0 +1,9 @@
+#define HELP_MESSAGE \
+"usage: superkb [options]\n\
+\n\
+Options:\n\
+        -0         Quit when Superkb is ready (for timing and debugging).\n\
+        -d level   Show debug messages up to the specified verbosity level.\n\
+        -h         Shows this help.\n\
+        -v         Shows program version.\n\
+"
